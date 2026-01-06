a289ea847adb316fe0c4ee79d1cfeff12cbd306c
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2026-01-06 09:39:00 -0500

    combining landings trends for all species into csv

--- scripts/other/SA_combined_code.R
+++ scripts/other/SA_combined_code.R
@@ -200,4 +200,127 @@ combined_rec <- combined_rec %>% arrange(year)
 write.csv(combined_rec, file = file.path(output_folder, "combined_recreational_ratio_trends.csv"), row.names = FALSE)
 
 
+
+
+
+
+### Commercial landings ###
+
+# Set the folder where .csv files are stored
+input_folder = here("data/formatted/formatted_csvs")
+
+output_folder = here("data/intermediate")
+
+# List all csv files that contain SEDAR and Biomass
+files <- list.files(input_folder, full.names = TRUE)
+files <- files[grepl("SEDAR", files) & grepl("Commercial", files) & grepl("Catch", files)]
+
+# Read and combine
+com_df_list <- map(files, function(file) {
+  
+  dat <- read.csv(file, stringsAsFactors = FALSE)
+  
+  dat = dat %>% 
+    row_to_names(row_number = 2) %>% 
+    rename(year = extent) 
+  
+  target_col = "commercial_landings"
+  
+  if (target_col %in% names(dat)) {
+    
+    # Extract species_assessment from filename
+    filename <- basename(file)
+    name <- str_remove(filename, "_Commercial_Catch_formatted\\.csv$")  # remove suffix
+    
+    dat = dat %>% 
+      select(year, all_of(target_col)) %>% 
+      mutate(year = as.integer(year), 
+             !!target_col := as.numeric(get(target_col))) %>% 
+      rename(!!name := target_col)
+    
+    return(dat)
+    
+  } else {
+    
+    # Column does NOT exist: Return NULL to ignore this file
+    message(paste("File ignored (missing column):", basename(file)))
+    return(NULL)
+    
+  }
+  
+})
+
+com_df_list = purrr::compact(com_df_list)
+
+# Merge all data frames by Yr
+# Reduce joins all the data frames by Yr column
+combined_com <- reduce(com_df_list, full_join, by = "year")
+
+# Arrange by year
+combined_com <- combined_com %>% arrange(year)
+
+write.csv(combined_com, file = file.path(output_folder, "combined_commercial_landings_trends.csv"), row.names = FALSE)
+
+
+
+
+
+### Recreational landings:discards ratio ###
+
+# Set the folder where .csv files are stored
+input_folder = here("data/formatted/formatted_csvs")
+
+output_folder = here("data/intermediate")
+
+# List all csv files that contain SEDAR and Biomass
+files <- list.files(input_folder, full.names = TRUE)
+files <- files[grepl("SEDAR", files) & grepl("Recreational", files) & grepl("Catch", files)]
+
+# Read and combine
+rec_df_list <- map(files, function(file) {
+  
+  dat <- read.csv(file, stringsAsFactors = FALSE)
+  
+  dat = dat %>% 
+    row_to_names(row_number = 2) %>% 
+    rename(year = extent) 
+  
+  target_col = "recreational_landings"
+  
+  if (target_col %in% names(dat)) {
+    
+    # Extract species_assessment from filename
+    filename <- basename(file)
+    name <- str_remove(filename, "_Recreational_Catch_formatted\\.csv$")  # remove suffix
+    
+    dat = dat %>% 
+      select(year, all_of(target_col)) %>% 
+      mutate(year = as.integer(year), 
+             !!target_col := as.numeric(get(target_col))) %>% 
+      rename(!!name := target_col)
+    
+    return(dat)
+    
+  } else {
+    
+    # Column does NOT exist: Return NULL to ignore this file
+    message(paste("File ignored (missing column):", basename(file)))
+    return(NULL)
+    
+  }
+  
+})
+
+rec_df_list = purrr::compact(rec_df_list)
+
+# Merge all data frames by Yr
+# Reduce joins all the data frames by Yr column
+combined_rec <- reduce(rec_df_list, full_join, by = "year")
+
+# Arrange by year
+combined_rec <- combined_rec %>% arrange(year)
+
+write.csv(combined_rec, file = file.path(output_folder, "combined_recreational_landings_trends.csv"), row.names = FALSE)
+
+
 #END SCRIPT
